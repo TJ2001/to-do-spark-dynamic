32f299ff7d3ce1ea4aef2e51fa4cfe9b36042913
Joanna Anderson and Tim Jung <joanna@>
2016-09-13 15:46:02 -0700

    add date functionality

--- src/main/java/ToDo.java
+++ src/main/java/ToDo.java
@@ -1,11 +1,47 @@
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ToDo {
   private String mDescription;
+  private boolean mCompleted;
+  private LocalDateTime mCreatedAt;
+  private static List<ToDo> instances = new ArrayList<ToDo>();
+  private int mId;
 
   public ToDo(String description){
     mDescription = description;
+    mCompleted = false;
+    mCreatedAt = LocalDateTime.now();
+    instances.add(this);
+    mId = instances.size();
   }
 
   public String getDescription() {
     return mDescription;
+  }
+
+  public boolean isCompleted() {
+    return mCompleted;
+  }
+
+  public LocalDateTime getCreatedAt() {
+    return mCreatedAt;
+  }
+
+  public static List<ToDo> all() {
+    return instances;
+  }
+
+  public static void clear() {
+    instances.clear();
+  }
+
+  public int getId() {
+    return mId;
+  }
+
+  public static ToDo find(int id) {
+    return instances.get(id - 1);
   }
 }
--- src/test/ToDoTest.java
+++ src/test/ToDoTest.java
@@ -1,5 +1,6 @@
 import org.junit.*;
 import static org.junit.Assert.*;
+import java.time.LocalDateTime;
 
 public class ToDoTest {
 
@@ -13,5 +14,48 @@ public class ToDoTest {
   public void ToDo_instantiatesWithDescription_String(){
     ToDo myToDo = new ToDo("Cook Curry");
     assertEquals("Cook Curry", myToDo.getDescription());
+  }
+
+  @Test
+  public void isCompleted_isFalseAfterInstantiation_false() {
+    ToDo myToDo = new ToDo("Mow the lawn");
+    assertEquals(false, myToDo.isCompleted());
+  }
+
+  @Test
+  public void getCreatedAt_instantiatesWithCurrentTime_today(){
+    ToDo myToDo = new ToDo("Mow the Lawn");
+    assertEquals(LocalDateTime.now().getDayOfWeek(), myToDo.getCreatedAt().getDayofWeek());
+  }
+
+  @Test
+  public void all_returnsAllInstancesOfTask_true() {
+    ToDo firstToDo = new ToDo("Mow the lawn");
+    ToDo secondToDo = new ToDo("Buy groceries");
+    assertEquals(true, ToDo.all().contains(firstToDo));
+    assertEquals(true, ToDo.all().contains(secondToDo));
+  }
+
+  @Test
+  public void clear_emptiesAllTasksFromArrayList_0() {
+    ToDo myToDo = new ToDo("Mow the lawn");
+    ToDo.clear();
+    assertEquals(ToDo.all().size(), 0);
+// Does it matter, that zero and methods are in different orders?
+
+  }
+
+  @Test
+  public void getId_tasksInstantiateWithAnID_1() {
+    ToDo.clear();
+    ToDo myToDo = new ToDo("Mow the lawn");
+    assertEquals(1, myToDo.getId());
+  }
+
+  @Test
+  public void find_returnsToDoWithSameId_secondToDo(){
+    ToDo firstToDo = new ToDo("Mow the lawn")
+    ToDo secondToDo = new ToDo("Buy the groceries");
+    assertEquals(Task.find(secondToDo.getId()), secondToDo);
   }
 }

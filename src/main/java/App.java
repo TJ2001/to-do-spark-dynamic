2d97706724d78b9c19f10044b833a5d2808873a2
Joanna Anderson and Tim Jung <joanna@>
2016-09-13 16:02:38 -0700

    add new methods for App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -13,22 +13,28 @@ public class App {
 
     get("/", (request, response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
-      model.put("todos", request.session().attribute("todos"));
+      // model.put("todos", request.session().attribute("todos"));
       model.put("template", "templates/index.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
-    post("/todo", (request, response) -> {
+    get("toDos/new", (request, response) -> {
+      Map<String, Object> model = new HashMap<String,Object>();
+      model.put("template", "templates/task-form.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/toDos", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("toDos", ToDo.all());
+      model.put("template", "templates/tasks.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post("/toDos", (request, response) -> {
       Map<String, Object> model  = new HashMap<String, Object>();
-      ArrayList<ToDo> todos = request.session().attribute("todos");
-      if (todos == null) {
-        todos = new ArrayList<ToDo>();
-        request.session().attribute("todos", todos);
-      }
       String description = request.queryParams("description");
       ToDo newToDo = new ToDo(description);
-      todos.add(newToDo);
-      request.session().attribute("todo", newToDo);
       model.put("template", "templates/success.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
